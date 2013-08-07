9a6084d9f59012764c18b2fe34441e8e9ea45c38
Daniel Lacroix <dlacroix@erasme.org>
2013-08-07 16:09:59 +0200

    Change copyright

--- TestHttpServer/AssemblyInfo.cs
+++ TestHttpServer/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("daniel")]
+[assembly: AssemblyCopyright("Departement du Rhone")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
