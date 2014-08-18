1b39e657dbec4e375b6735c828b5cd18b402e6da
Daniel Lacroix <dlacroix@erasme.org>
2014-08-18 15:54:58 +0200

    update copyright

--- Erasme.Http/Erasme.Http/HttpContext.cs
+++ Erasme.Http/Erasme.Http/HttpContext.cs
@@ -6,7 +6,7 @@
 // Author(s):
 //  Daniel Lacroix <dlacroix@erasme.org>
 // 
-// Copyright (c) 2013 Departement du Rhone
+// Copyright (c) 2013-2014 Departement du Rhone
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
