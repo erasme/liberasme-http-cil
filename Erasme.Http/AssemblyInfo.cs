b6096355759293ceefe53a9a6b7417cbf3f66143
Daniel Lacroix <dlacroix@erasme.org>
2015-03-12 17:09:42 +0100

    update changelog and version

--- Erasme.Http/AssemblyInfo.cs
+++ Erasme.Http/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Departement du Rhone - ERASME")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("(c) 2013. Departement du Rhone")]
+[assembly: AssemblyCopyright("(c) 2013-2015. Departement du Rhone - Metropole de Lyon")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -18,7 +18,7 @@ using System.Runtime.CompilerServices;
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
