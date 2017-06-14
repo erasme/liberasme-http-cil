f0efe169e800aa511109a90d129fbc0317785562
Daniel LACROIX <dlacroix@erasme.org>
2017-06-14 17:21:55 +0200

    Profile methods to convert simple object to JSON and to create simple object for JSON

--- Erasme.Http/Erasme.Json/JsonValue.cs
+++ Erasme.Http/Erasme.Json/JsonValue.cs
@@ -258,6 +258,101 @@ namespace Erasme.Json
 					dest[key] = source[key];
 			}
 		}
+
+		public T ToObject<T>() where T : new()
+		{
+			var result = new T();
+			foreach (var field in typeof(T).GetFields())
+			{
+				if (!ContainsKey(field.Name))
+					continue;
+
+				var value = this[field.Name];
+				if (value is JsonPrimitive)
+					field.SetValue(result, Convert.ChangeType(value.Value, field.FieldType));
+				else if (value is JsonObject)
+					field.SetValue(result, GetType().GetMethod(nameof(ToObject)).MakeGenericMethod(field.FieldType).Invoke(value, new object[] { }));
+			}
+			foreach (var prop in typeof(T).GetProperties())
+			{
+				if (!ContainsKey(prop.Name))
+					continue;
+
+				var value = this[prop.Name];
+				if (value is JsonPrimitive)
+					prop.SetValue(result, Convert.ChangeType(value.Value, prop.PropertyType));
+				else if (value is JsonObject)
+					prop.SetValue(result, GetType().GetMethod(nameof(ToObject)).MakeGenericMethod(prop.PropertyType).Invoke(value, new object[] { }));
+			}
+			return result;
+		}
+
+		public static T ParseToObject<T>(string jsonString) where T : new()
+		{
+			var deserializer = new JsonDeserializer();
+			var json = deserializer.Deserialize(jsonString);
+			return json.ToObject<T>();
+		}
+
+		static JsonValue NativeToJsonValue(object value)
+		{
+			JsonValue result = null;
+			if (value is string)
+				result = (string)value;
+			else if (value is int)
+				result = (int)value;
+			else if (value is int?)
+				result = (int?)value;
+			else if (value is uint)
+				result = (uint)value;
+			else if (value is uint?)
+				result = (uint?)value;
+			else if (value is long)
+				result = (long)value;
+			else if (value is long?)
+				result = (long?)value;
+			else if (value is ulong)
+				result = (ulong)value;
+			else if (value is ulong?)
+				result = (ulong?)value;
+			else if (value is float)
+				result = (float)value;
+			else if (value is float?)
+				result = (float?)value;
+			else if (value is double)
+				result = (double)value;
+			else if (value is double?)
+				result = (double?)value;
+			else if (value is bool)
+				result = (bool)value;
+			else if (value is bool?)
+				result = (bool?)value;
+			else if (value is DateTime)
+				result = (DateTime)value;
+			else if (value is DateTime?)
+				result = (DateTime?)value;
+			else if (value is TimeSpan)
+				result = ((TimeSpan)value).TotalSeconds;
+			else if (value as object == null)
+				result = null;
+			else if (value.GetType().IsClass)
+				result = ObjectToJson(value);
+			return result;
+		}
+
+		public static JsonObject ObjectToJson(object obj)
+		{
+			var result = new JsonObject();
+			foreach (var field in obj.GetType().GetFields())
+				result[field.Name] = NativeToJsonValue(field.GetValue(obj));
+			foreach (var prop in obj.GetType().GetProperties())
+			{
+				if (prop.GetIndexParameters().Length > 0)
+					continue;
+				result[prop.Name] = NativeToJsonValue(prop.GetValue(obj));
+			}
+			return result;
+		}
 	}
 }
 
