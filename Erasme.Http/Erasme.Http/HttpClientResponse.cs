1834f2ab6a03bedc145ae41b8f8ea58b54ba9405
daniel <dlacroix@erasme.org>
2019-01-18 16:38:51 +0100

    correct a bug in ReadAsStringAsync which dispose the stream reader possibly before the end of reading. This can create exception when async reading string (or JSON) HttpClientResponse

--- Erasme.Http/Erasme.Http/HttpClientResponse.cs
+++ Erasme.Http/Erasme.Http/HttpClientResponse.cs
@@ -35,196 +35,209 @@ using Erasme.Json;
 
 namespace Erasme.Http
 {
-	public class HttpClientResponse
-	{
-		public HttpClientResponse(string status, HttpHeaders headers, BufferContext bufferContext)
-		{
-			HttpUtility.ParseStatus(status, out protocol, out statusCode, out statusDescription);
-			Headers = headers;
-			if(Headers.ContainsKey("cookie"))
-				Cookies = HttpUtility.ParseCookie(Headers["cookie"]);
-			else
-				Cookies = new Dictionary<string, string>();
+    public class HttpClientResponse
+    {
+        public HttpClientResponse(string status, HttpHeaders headers, BufferContext bufferContext)
+        {
+            HttpUtility.ParseStatus(status, out protocol, out statusCode, out statusDescription);
+            Headers = headers;
+            if (Headers.ContainsKey("cookie"))
+                Cookies = HttpUtility.ParseCookie(Headers["cookie"]);
+            else
+                Cookies = new Dictionary<string, string>();
 
-			if(Headers.ContainsKey("content-length")) {
-				long contentLength = Convert.ToInt64(Headers["content-length"]);
-				InputStream = new LengthLimitedStream(bufferContext, contentLength);
-			}
-			else if(Headers.ContainsKey("transfer-encoding") && (Headers["transfer-encoding"].ToLower() == "chunked"))
-				InputStream = new InputChunkedStream(bufferContext);
-			else
-				InputStream = new LengthLimitedStream(bufferContext, 0);
-		}
+            if (Headers.ContainsKey("content-length"))
+            {
+                long contentLength = Convert.ToInt64(Headers["content-length"]);
+                InputStream = new LengthLimitedStream(bufferContext, contentLength);
+            }
+            else if (Headers.ContainsKey("transfer-encoding") && (Headers["transfer-encoding"].ToLower() == "chunked"))
+                InputStream = new InputChunkedStream(bufferContext);
+            else
+                InputStream = new LengthLimitedStream(bufferContext, 0);
+        }
 
-		/// <summary>
-		/// Gets or sets the headers of the response.
-		/// </summary>
-		/// <value>
-		/// The headers.
-		/// </value>
-		public HttpHeaders Headers { get; internal set; }
+        /// <summary>
+        /// Gets or sets the headers of the response.
+        /// </summary>
+        /// <value>
+        /// The headers.
+        /// </value>
+        public HttpHeaders Headers { get; internal set; }
 
-		/// <summary>
-		/// Gets or sets the cookies to return with the response.
-		/// </summary>
-		/// <value>
-		/// The cookies.
-		/// </value>
-		public Dictionary<string,string> Cookies { get; internal set; }
+        /// <summary>
+        /// Gets or sets the cookies to return with the response.
+        /// </summary>
+        /// <value>
+        /// The cookies.
+        /// </value>
+        public Dictionary<string, string> Cookies { get; internal set; }
 
-		string protocol;
-		/// <summary>
-		/// Gets the HTTP protocol.
-		/// </summary>
-		/// <value>
-		/// The protocol.
-		/// </value>
-		public string Protocol {
-			get {
-				return protocol;
-			}
-		}
+        string protocol;
+        /// <summary>
+        /// Gets the HTTP protocol.
+        /// </summary>
+        /// <value>
+        /// The protocol.
+        /// </value>
+        public string Protocol
+        {
+            get
+            {
+                return protocol;
+            }
+        }
 
-		int statusCode;
-		/// <summary>
-		/// Gets or sets the HTTP status code of the response.
-		/// </summary>
-		/// <value>
-		/// The status code.
-		/// </value>
-		public int StatusCode {
-			get {
-				return statusCode;
-			}
-		}
+        int statusCode;
+        /// <summary>
+        /// Gets or sets the HTTP status code of the response.
+        /// </summary>
+        /// <value>
+        /// The status code.
+        /// </value>
+        public int StatusCode
+        {
+            get
+            {
+                return statusCode;
+            }
+        }
 
-		string statusDescription;
-		/// <summary>
-		/// Gets or sets the HTTP status description of the response.
-		/// </summary>
-		/// <value>
-		/// The status description.
-		/// </value>
-		public string StatusDescription {
-			get {
-				return statusDescription;
-			}
-		}
+        string statusDescription;
+        /// <summary>
+        /// Gets or sets the HTTP status description of the response.
+        /// </summary>
+        /// <value>
+        /// The status description.
+        /// </value>
+        public string StatusDescription
+        {
+            get
+            {
+                return statusDescription;
+            }
+        }
 
-		public string Status {
-			get {
-				if(StatusDescription == String.Empty)
-					return StatusCode+" "+HttpUtility.GetStatusDetail(StatusCode);
-				else
-					return StatusCode+" "+StatusDescription;
-			}
-		}
+        public string Status
+        {
+            get
+            {
+                if (StatusDescription == String.Empty)
+                    return StatusCode + " " + HttpUtility.GetStatusDetail(StatusCode);
+                else
+                    return StatusCode + " " + StatusDescription;
+            }
+        }
 
-		/// <summary>
-		/// If the HTTP request has a body (for POST or PUT request),
-		/// gets the body input stream.
-		/// </summary>
-		/// <value>
-		/// The input stream.
-		/// </value>
-		public Stream InputStream { get; private set; }
+        /// <summary>
+        /// If the HTTP request has a body (for POST or PUT request),
+        /// gets the body input stream.
+        /// </summary>
+        /// <value>
+        /// The input stream.
+        /// </value>
+        public Stream InputStream { get; private set; }
 
-		/// <summary>
-		/// Convenient method to read the InputStream and return a string
-		/// </summary>
-		/// <returns>
-		/// The string.
-		/// </returns>
-		public string ReadAsString()
-		{
-			using(StreamReader reader = new StreamReader(InputStream, Encoding.UTF8))
-				return reader.ReadToEnd();
-		}
+        /// <summary>
+        /// Convenient method to read the InputStream and return a string
+        /// </summary>
+        /// <returns>
+        /// The string.
+        /// </returns>
+        public string ReadAsString()
+        {
+            using (StreamReader reader = new StreamReader(InputStream, Encoding.UTF8))
+                return reader.ReadToEnd();
+        }
 
-		/// <summary>
-		/// Convenient method to read the InputStream and return a string
-		/// </summary>
-		/// <returns>
-		/// The string async.
-		/// </returns>
-		public Task<string> ReadAsStringAsync()
-		{
-			using(StreamReader reader = new StreamReader(InputStream, Encoding.UTF8))
-				return reader.ReadToEndAsync();
-		}
+        /// <summary>
+        /// Convenient method to read the InputStream and return a string
+        /// </summary>
+        /// <returns>
+        /// The string async.
+        /// </returns>
+        public async Task<string> ReadAsStringAsync()
+        {
+            using (StreamReader reader = new StreamReader(InputStream, Encoding.UTF8))
+                return await reader.ReadToEndAsync();
+        }
 
-		/// <summary>
-		/// Convenient method to read the InputStream and return a byte array
-		/// </summary>
-		/// <returns>
-		/// The byte array.
-		/// </returns>
-		public byte[] ReadAsBytes()
-		{
-			MemoryStream stream;
-			try {
-				long length = InputStream.Length;
-				stream = new MemoryStream((int)length);
-			}
-			catch(NotSupportedException) {
-				stream = new MemoryStream();
-			}
-			InputStream.CopyTo(stream);
-			byte[] buffer;
-			if(stream.GetBuffer().LongLength != stream.Length) {
-				buffer = new byte[stream.Length];
-				Array.Copy(stream.GetBuffer(), buffer, (int)stream.Length);
-			}
-			else
-				buffer = stream.GetBuffer();
-			return buffer;
-		}
+        /// <summary>
+        /// Convenient method to read the InputStream and return a byte array
+        /// </summary>
+        /// <returns>
+        /// The byte array.
+        /// </returns>
+        public byte[] ReadAsBytes()
+        {
+            MemoryStream stream;
+            try
+            {
+                long length = InputStream.Length;
+                stream = new MemoryStream((int)length);
+            }
+            catch (NotSupportedException)
+            {
+                stream = new MemoryStream();
+            }
+            InputStream.CopyTo(stream);
+            byte[] buffer;
+            if (stream.GetBuffer().LongLength != stream.Length)
+            {
+                buffer = new byte[stream.Length];
+                Array.Copy(stream.GetBuffer(), buffer, (int)stream.Length);
+            }
+            else
+                buffer = stream.GetBuffer();
+            return buffer;
+        }
 
-		/// <summary>
-		/// Convenient method to read the InputStream and return a byte array
-		/// </summary>
-		/// <returns>
-		/// The byte array async.
-		/// </returns>
-		public async Task<byte[]> ReadAsBytesAsync()
-		{
-			byte[] buffer = new byte[InputStream.Length];
-			int offset = 0;
-			int count = 0;
-			int size;
-			do {
-				size = await InputStream.ReadAsync(buffer, offset, (int)InputStream.Length - count);
-				count += size;
-			} while((size > 0) &&(count < InputStream.Length));
-			return buffer;
-		}
+        /// <summary>
+        /// Convenient method to read the InputStream and return a byte array
+        /// </summary>
+        /// <returns>
+        /// The byte array async.
+        /// </returns>
+        public async Task<byte[]> ReadAsBytesAsync()
+        {
+            byte[] buffer = new byte[InputStream.Length];
+            int offset = 0;
+            int count = 0;
+            int size;
+            do
+            {
+                size = await InputStream.ReadAsync(buffer, offset, (int)InputStream.Length - count);
+                count += size;
+            } while ((size > 0) && (count < InputStream.Length));
+            return buffer;
+        }
 
-		/// <summary>
-		/// Convenient method to read the InputStream and return a JsonValue
-		/// </summary>
-		/// <returns>
-		/// The JsonValue.
-		/// </returns>
-		public JsonValue ReadAsJson()
-		{
-			return JsonValue.Parse(ReadAsString());
-		}
+        /// <summary>
+        /// Convenient method to read the InputStream and return a JsonValue
+        /// </summary>
+        /// <returns>
+        /// The JsonValue.
+        /// </returns>
+        public JsonValue ReadAsJson()
+        {
+            return JsonValue.Parse(ReadAsString());
+        }
 
-		/// <summary>
-		/// Convenient method to read the InputStream and return a JsonValue
-		/// </summary>
-		/// <returns>
-		/// The JsonValue async.
-		/// </returns>
-		public async Task<JsonValue> ReadAsJsonAsync()
-		{
-			return JsonValue.Parse(await ReadAsStringAsync());
-		}
+        /// <summary>
+        /// Convenient method to read the InputStream and return a JsonValue
+        /// </summary>
+        /// <returns>
+        /// The JsonValue async.
+        /// </returns>
+        public async Task<JsonValue> ReadAsJsonAsync()
+        {
+            return JsonValue.Parse(await ReadAsStringAsync());
+        }
 
-		public MultipartReader ReadAsMultipart()
-		{
-			return new MultipartReader(InputStream, Headers["content-type"]);
-		}
-	}
+        public MultipartReader ReadAsMultipart()
+        {
+            return new MultipartReader(InputStream, Headers["content-type"]);
+        }
+    }
 }
 
